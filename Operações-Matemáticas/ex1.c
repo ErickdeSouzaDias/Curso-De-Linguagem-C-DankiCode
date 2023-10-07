c1fcd62b1210b8010f3f39a4636afa8fa5bcd1a2
Érick De Souza Dias <erickdesouzadias072@gmail.com>
2023-10-07 10:26:01 -0300

    Adicionado exercício 1.
    Faça um programa que some três valores e mostre o resultado na tela.

---
+++ Operações-Matemáticas/ex1.c
@@ -0,0 +1,19 @@
+// Faça um programa que some três valores e mostre o resultado na tela.
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    // Armazenando os valores nas variáveis.
+    int num1 = 2;
+    int num2 = 4;
+    int num3 = 6;
+
+    // Somando os valores das variáveis
+    int soma = num1 + num2 + num3;
+
+    // Exibindo a soma dos valores das variáveis.
+    printf("A soma %d + %d + %d é igual a %d.", num1, num2, num3, soma);
+
+    return 0;
+}
